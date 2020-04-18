3904e76eb0ed2cda41e4b672447ecac92664f87d
Fele Omolola <lummy1997@gmail.com>
2020-04-18 14:40:51 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
-var dialLines = document.getElementsByClassName('diallines');
-var clockEl = document.getElementsByClassName('clock')[0];
-
+let analog = document.getElementsByClassName('analog');
+let clockAn = document.getElementsByClassName('clock')[0];
+//usng this loop for the necessary rotation
+//deg which is degree (it helps to rotate in degrees)
+//one full circle is 360 deg
 for (var i = 1; i < 60; i++) {
-  clockEl.innerHTML += "<div class='diallines'></div>";
-  dialLines[i].style.transform = "rotate(" + 6 * i + "deg)";
+  clockAn.innerHTML += "<div class='analog'></div>";
+  analog[i].style.transform = "rotate(" + 6 * i + "deg)";
 }
 
 function clock() {
-  var weekday = new Array(7),
+  let weekday = new Array(7),
+  //new Date, hours, minutes and seconds.
       d = new Date(),
       h = d.getHours(),
       m = d.getMinutes(),
@@ -19,13 +22,14 @@ function clock() {
       hDeg = h * 30 + m * (360/720),
       mDeg = m * 6 + s * (360/3600),
       sDeg = s * 6,
-      
-      hEl = document.querySelector('.hour-hand'),
-      mEl = document.querySelector('.minute-hand'),
-      sEl = document.querySelector('.second-hand'),
-      dateEl = document.querySelector('.date'),
-      dayEl = document.querySelector('.day');
-
+      //the hour,minute,date, seconds and day changes at the right time
+      hourAn = document.querySelector('.hour-tick'),
+      minuteAn = document.querySelector('.minute-tick'),
+      secondsAn = document.querySelector('.second-tick'),
+      dateAn = document.querySelector('.dateclock'),
+      dayAn = document.querySelector('.dayclock');
+     
+      //the day changes
       weekday[0] = "Sunday";
       weekday[1] = "Monday";
       weekday[2] = "Tuesday";
@@ -34,17 +38,17 @@ function clock() {
       weekday[5] = "Friday";
       weekday[6] = "Saturday";
   
-      var day = weekday[d.getDay()];
+      let day = weekday[d.getDay()];
   
   if(month < 9) {
     month = "0" + month;
   }
   
-  hEl.style.transform = "rotate("+hDeg+"deg)";
-  mEl.style.transform = "rotate("+mDeg+"deg)";
-  sEl.style.transform = "rotate("+sDeg+"deg)";
-  dateEl.innerHTML = date+"/"+month+"/"+year;
-  dayEl.innerHTML = day;
+  hourAn.style.transform = "rotate("+hDeg+"deg)";
+  minuteAn.style.transform = "rotate("+mDeg+"deg)";
+  secondsAn.style.transform = "rotate("+sDeg+"deg)";
+  dateAn.innerHTML = date+"/"+month+"/"+year;
+  dayAn.innerHTML = day;
 }
 
 setInterval("clock()", 100);
